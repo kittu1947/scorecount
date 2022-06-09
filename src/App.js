cf0907b4fd3fc117cb7949afe814a087ee4243d3
kittu1947 <modulus.krishna@googlemail.com>
2022-06-09 20:17:38 +0530

    score count app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
-import React,{useState} from "react";
-import Todolist from "./Todolist";
+import React, {useState,useEffect } from 'react';
+import "./App.css";
 
-const Todo =()=>{
-const [task,setTask]=useState("");
-const [todos,setTodos]=useState([]);
 
-  const changehandler =e=>{
-  setTask(e.target.value)
-}
-
-  const submithandler=e=>{
-    e.preventDefault();
-    const newtodos=[...todos,task];
-    setTodos(newtodos)
-    setTask("");
-  }
-  const deletehandler=(indexvalue)=>{
-  const newtodos=todos.filter((todos,index)=>
-  index!==indexvalue)
-setTodos(newtodos);
-}
+const App =()=>
+{
+  const[count,setCount]=useState(0);
+  useEffect(()=>console.log("clicked",[count]))
   return(
+    <center>
     <div>
-      <center>
-      <h4>TODO MANAGER</h4>
-      <form onSubmit={submithandler}>
-        <input type="text"  value={task} onChange={changehandler} /> &nbsp;&nbsp;
-        <input type="submit" value="ADD" name="ADD"/>   
-      </form>
-    <Todolist todolist={todos} delethandler={deletehandler}/>
-      
-      </center>
-    </div>
+      <h1>{count}</h1>
+      <button onClick={()=>setCount(count+1)}>SOCRE COUNT</button>
+    </div></center>
   )
+
 }
-export default Todo;
+export default App;
